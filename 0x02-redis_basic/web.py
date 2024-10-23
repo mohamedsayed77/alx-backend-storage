b781b5fc554891a0fb961643448e3a6addbf5a07
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2024-10-24 01:10:16 +0300

    redis basics

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
-'''A module with tools for request caching and tracking.
 '''
+A module with tools for request caching and tracking.
+'''
+
 import redis
 import requests
 from functools import wraps
 from typing import Callable
 
-
+# The module-level Redis instance.
 redis_store = redis.Redis()
-'''The module-level Redis instance.
-'''
-
 
 def data_cacher(method: Callable) -> Callable:
     '''Caches the output of fetched data.
@@ -19,16 +18,14 @@ def data_cacher(method: Callable) -> Callable:
     def invoker(url) -> str:
         '''The wrapper function for caching the output.
         '''
-        redis_store.incr(f'count:{url}')
+        redis_store.incr(f'count:{url}') # Increment the counter each time
         result = redis_store.get(f'result:{url}')
         if result:
             return result.decode('utf-8')
         result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
+        redis_store.setex(f'result:{url}', 10, result) # Cache result for 10 seconds
         return result
     return invoker
-
 
 @data_cacher
 def get_page(url: str) -> str:
