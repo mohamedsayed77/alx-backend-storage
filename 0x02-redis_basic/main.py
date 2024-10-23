ebae908d2eaa1a0316be6777aede1b8d677e1da7
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2024-10-24 00:58:20 +0300

    redis basics

--- 0x02-redis_basic/main.py
+++ 0x02-redis_basic/main.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
-""" Main file """
+"""
+Main file
+"""
+import redis
 
 Cache = __import__('exercise').Cache
 
 cache = Cache()
+
+# data = b"hello"
+# key = cache.store(data)
+# print(key)
+
+# local_redis = redis.Redis()
+# print(local_redis.get(key))
 
 cache.store(b"first")
 print(cache.get(cache.store.__qualname__))
